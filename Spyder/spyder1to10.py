ad8577efba3cf852bf806b54a2fe13ff6a7eec22
kennethnicholastt <114973245+kennethnicholastt@users.noreply.github.com>
2022-10-04 12:59:01 +0800

    Add files via upload

---
+++ Spyder/spyder1to10.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  4 12:49:37 2022
+
+@author: kennethnicholastanoyo
+"""
+
+
+numbers =[1,2,3,4,5,6,7,8,9,10]
+for number in numbers:
+    print("the current number is", number)
+    
